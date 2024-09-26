4facc201311b63ddc2bfcd45ae32ce8bf6b7aa4c
Adarsh Kumar Singh <142895773+Account1Adarsh@users.noreply.github.com>
2024-09-26 20:14:17 +0530

    Create program.cpp

---
+++ experiment 3/program.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+
+int main(){
+    string s;
+    cout<<"Enter a binary string"<<endl;
+    cin>>s;
+    int n=s.length();
+    bool flag=false;
+    for(int i=0;i<n;i++){
+        if(s[i]=='0' or s[i]=='1'){
+
+        }
+        else{
+            flag=true;
+            break;
+        }
+    }
+    if(flag){
+        cout<<"INVALID STRING"<<endl;
+    }
+    else{
+        int i=0;
+        char curr='a';
+        while(i<n){
+            if(curr=='a'){
+                if(s[i]=='1') curr='b';
+            }
+            else if(curr='b'){
+                if(s[i]=='0'){
+                    curr='c';
+                }
+                else{
+                    curr='a';
+                }
+            }
+            else{
+                if(s[i]=='0') curr='a';
+            }
+        }
+        if(curr=='a'){
+            cout<<"Yes , its correct and acceptable input"<<endl;
+        }
+        else{
+            cout<<"Not acceptable"<<endl;
+        }
+    }
+}
