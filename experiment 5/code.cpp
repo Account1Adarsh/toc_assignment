fd1ab1fd7b5b7f4116ea0171a0b11d649b1856fa
Adarsh Kumar Singh <142895773+Account1Adarsh@users.noreply.github.com>
2024-10-15 08:24:26 +0000

    expreiment 5 and 6

---
+++ experiment 5/code.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter the string: "<<endl;
+    string s;
+    cin>>s;
+    int z=0;
+    int two=0;
+    int extra=0;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='0') z++;
+        else if(s[i]=='1') one++;
+        else extra++;
+    }
+    if(extra){
+        cout<<"Invalid input "<<endl;
+    }
+    else{
+        if(one==z) cout<<"Acceptable "<<endl;
+        else cout<<"Not acceptable "<<endl;
+    }
+}
