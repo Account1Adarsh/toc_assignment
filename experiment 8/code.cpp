99bdf0baa3a4096529ebe1a562a714008245611d
Adarsh Kumar Singh <142895773+Account1Adarsh@users.noreply.github.com>
2024-11-05 08:45:39 +0000

    update

---
+++ experiment 8/code.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Enter the string: "<<endl;
+    string s;
+    cin>>s;
+    int z=0;
+    int two=0;
+    int extra=0;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='0') z++;
+        else if(s[i]=='1') two++;
+        else extra++;
+    }
+    if(extra){
+        cout<<"Invalid input "<<endl;
+    }
+    else{
+        bool flag=false;
+        for(int i=s.length()-1;i>=0;i--){
+            if(flag==false){
+                if(s[i]=='0'){
+                    s[i]='1';
+                    flag=true;
+                }
+                else s[i]='0';
+            }
+            else break;
+        }
+        cout<<"Increament of the input string is: "<<s<<endl;
+    }
+}
